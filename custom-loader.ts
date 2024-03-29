1667ac3f57460a59340ac201ba097956a4e6e26e
Jeongjin Oh <rojay.developer@gmail.com>
2024-03-29 14:39:46 +0900

    fix: url returned by customLoader

--- custom-loader.ts
+++ custom-loader.ts
@@ -3,7 +3,5 @@
 import { ImageLoaderProps } from 'next/image';
 
 export function customLoader({ src, width, quality }: ImageLoaderProps) {
-  return `/_next/image?url=${encodeURIComponent(src)}&w=${width}&q=${
-    quality || 75
-  }`;
+  return `${src}?url=${encodeURIComponent(src)}&w=${width}&q=${quality || 75}`;
 }
